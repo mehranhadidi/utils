8d893ffcde2057280d957a5f155ea5725e2e890b
Mehran <mehran.hadidi@gmail.com>
2016-12-31 14:02:23 +0330

    call service added

--- src/SHR/CallService.php
+++ src/SHR/CallService.php
@@ -2,10 +2,63 @@
 
 namespace SHR;
 
+use GuzzleHttp\Client;
+
 class CallService
 {
-	public static function send()
-	{
-		return 'send method called ! :)';
-	}
+	const POST = 'POST';
+    const GET = 'GET';
+
+    public static function send($type, $url, $token = null, $headers = [], $checkException = true)
+    {
+        // default headers for every request
+        $defaultHeaders = [
+            'Accept' => 'application/json',
+            'Content-Type', 'text/json'
+        ];
+
+        // check if user passed a token, send it as Authorization Bearer header
+        if( ! is_null($token))
+            $defaultHeaders['Authorization'] = 'Bearer ' . $token;
+
+
+        // combine user custom headers with defaults
+        $headers = array_merge($headers, $defaultHeaders);
+
+        if($checkException)
+        {
+            try
+            {
+                $client = new Client([
+                    'headers' => $headers
+                ]);
+
+                // send user token to user-management
+                $response = $client->request($type, $url);
+
+                // response data as string
+                $data = $response->getBody()->getContents();
+
+                // if data is json return the object, if not return as string
+                if(self::isJson($data))
+                    return response(json_decode($data, true), $response->getStatusCode())
+                        ->header('Content-Type', 'text/json');
+                else
+                    return $data;
+            }
+            catch (\Exception $e)
+            {
+                $error = $e->getResponse()->getBody(true);
+
+                return response($error, $e->getCode())
+                    ->header('Content-Type', 'text/json');
+            }
+        }
+    }
+
+    private static function isJson($string)
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
 }
