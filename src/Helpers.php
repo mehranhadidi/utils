5dddb17e81724fb2df9a1529d9f663b17efb94b5
Mehran <mehran.hadidi@gmail.com>
2017-01-08 16:43:33 +0330

    validation error added

--- src/Helpers.php
+++ src/Helpers.php
@@ -9,3 +9,17 @@ if( ! function_exists('user'))
         return \Request::get('authenticated_user');
     }
 }
+
+/**
+ * Return a JSON response with a custom message & validation errors.
+ */
+if( ! function_exists('validationError'))
+{
+    function validationError($errors, $message = null)
+    {
+        return response()->json([
+            'message' => $message ?: trans('response.validation_error'),
+            'errors'  => $errors,
+        ], 422);
+    }
+}
